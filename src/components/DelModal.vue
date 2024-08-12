e3d20949f6ff89b4b76c60ffed6f2b93aea128fe
applehung1 <applehung1@gmail.com>
2024-08-12 15:28:38 +0800

    Initial commit

---
+++ src/components/DelModal.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="modal" tabindex="-1" ref="modal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">刪除確認</h5>
+          <button type="button" class="btn-close" @click="hideModal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>確定要刪除這個產品嗎？</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" @click="hideModal">取消</button>
+          <button type="button" class="btn btn-danger" @click="confirmDelete">確認刪除</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import modalMixin from '@/mixins/modalMixin'
+export default {
+  methods: {
+    confirmDelete () {
+      console.log('confirmDelete called')
+      this.$emit('del-item')
+      this.hideModal()
+    }
+  },
+  mixins: [modalMixin]
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="container mt-5">
+        <form class="row justify-content-center"
+        @submit.prevent="signIn">
+        <div class="col-md-6">
+            <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
+            <div class="mb-2">
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input
+                type="email"
+                id="inputEmail"
+                class="form-control"
+                placeholder="Email address"
+                required
+                autofocus
+                v-model="user.username"
+            />
+            </div>
+            <div class="mb-2">
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input
+                type="password"
+                id="inputPassword"
+                class="form-control"
+                placeholder="Password"
+                required
+                v-model="user.password"
+            />
+            </div>
+            <div class="text-end mt-4">
+            <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+            </div>
+        </div>
+        </form>
+    </div>
+</template>
+<script>
+import mitt from 'mitt'
+const emitter = mitt()
+export default {
+  data () {
+    return {
+      user: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  // inject: ['emitter'],
+  methods: {
+    signIn () {
+      console.log('login')
+      const api = `${process.env.VUE_APP_API}admin/signin`
+      console.log(api)
+      this.isLoading = true
+      this.$http.post(api, this.user)
+        .then((res) => {
+          this.isLoading = false
+          if (res.data.success) {
+            const { token, expired } = res.data
+            document.cookie = `hexToken=${token}; expires=${new Date(expired).toUTCString()}`
+            console.log(res)
+            console.log('emitter:', emitter)
+            emitter.emit('push-message', {
+              style: 'success',
+              title: '登入成功'
+            })
+            this.$router.push('/dashboard/products')
+            // 跳轉到dashboard頁面
+          } else {
+            emitter.emit('push-message', {
+              style: 'danger',
+              title: '登入失敗',
+              content: res.data.message.join('、')
+            })
+          }
+        })
+    }
+  }
+}
+</script>
